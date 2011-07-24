8941c7f2ee5fa471443a3f2a929d66c52b3ab1d8
Aaron Heidgerken <aheidgerken@smm.org>
2011-07-24 18:55:16 -0500

    Minor changes to the block setup

--- arduino_make/applet/arduino_make.cpp
+++ arduino_make/applet/arduino_make.cpp
@@ -1,31 +1,18 @@
 #include "WProgram.h"
 void setup();
 void loop();
-#include "fishMove.h"
-
 
 bool thru=false;
 
 void setup(){
-	flipper.setup();
-	flipper.setup();
 
 }
 
 void loop(){
-    flipper.start();
-  flipper.start();
-
-  if(!thru){
-    for(int i=0; i<5; i++){	flipper.manual("LEFT");
-	delay(1 *1000);
-	flipper.manual("RIGHT");
-	delay(0.5 *1000);
-}
-
-  }
-    
   
-
+  if(!thru){
+    
+  }
+  
   thru=true;
 }
--- arduino_make/arduino_make.pde
+++ arduino_make/arduino_make.pde
@@ -1,28 +1,15 @@
-#include "fishMove.h"
-
 
 bool thru=false;
 
 void setup(){
-	flipper.setup();
-	flipper.setup();
 
 }
 
 void loop(){
-    flipper.start();
-  flipper.start();
-
-  if(!thru){
-    for(int i=0; i<5; i++){	flipper.manual("LEFT");
-	delay(1 *1000);
-	flipper.manual("RIGHT");
-	delay(0.5 *1000);
-}
-
-  }
-    
   
-
+  if(!thru){
+    
+  }
+  
   thru=true;
 }
