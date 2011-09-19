ccc27cc53423907d008099f7f0483c9b0e61ddba
Aaron Heidgerken <aheidgerken@smm.org>
2011-09-19 08:54:07 -0500

    Small changes to the arena robot

--- arduino_make/applet/arduino_make.cpp
+++ arduino_make/applet/arduino_make.cpp
@@ -1,13 +1,42 @@
 #include "WProgram.h"
 void setup();
 void loop();
+#include "fishMove.h"
+
+
+
 
 bool thru=false;
 
 void setup(){
-  pinMode(13,OUTPUT);
+	flipper.setup();
+	flipper.setup();
+	flipper.setup();
+	flipper.setup();
+
 }
 
 void loop(){
-  digitalWrite(13,(millis()/1000)%2);
+    flipper.start();
+  flipper.start();
+  flipper.start();
+  flipper.start();
+
+  if(!thru){
+    flipper.manual("RIGHT");
+delay(1 *1000);
+flipper.manual("LEFT");
+delay(1 *1000);
+flipper.manual("RIGHT");
+delay(1 *1000);
+flipper.manual("LEFT");
+delay(1 *1000);
+
+  }
+    
+  
+  
+  
+
+  thru=true;
 }
